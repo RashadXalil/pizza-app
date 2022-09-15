da835b6fc819e9d1a98a3386cbd88f9e0a7b644e
RashadXalil <rashadmkh@code.edu.az>
2022-09-15 10:23:04 +0400

    header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
+import Header from './components/Header'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
 function App() {
-  return <div className="App">hello world</div>
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" />
+          <Route path="/logout" />
+          <Route path="/login" />
+          <Route path="/register" />
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App
